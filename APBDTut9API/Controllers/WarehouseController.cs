db1f0a012a905c40bf0623ca2beccfbca1a4ac88
Bohdan Harasym <s31539@pjwstk.edu.pl>
2025-05-10 19:43:33 +0200

    Added WarehouseController and ApiService

---
+++ APBDTut9API/Controllers/WarehouseController.cs
@@ -0,0 +1,31 @@
+namespace APBDTut9API.Controllers;
+
+using APBDTut9API.Models;
+using APBDTut9API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WarehouseController : ControllerBase
+{
+    private readonly IApiService _service;
+
+    public WarehouseController(IApiService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddProductWarehouseAsync(DeliveryDTO delivery)
+    {
+        try
+        {
+            var id = await _service.AddProductWarehouseAsync(delivery);
+            return Created("", id);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
--- APBDTut9API/Services/ApiService.cs
+++ APBDTut9API/Services/ApiService.cs
@@ -1,6 +1,79 @@
+using System.Data.SqlClient;
+using APBDTut9API.Models;
+
 namespace APBDTut9API.Services;
 
-public class ApiService
+public class ApiService : IApiService
 {
-    
+    public readonly string _connectionString;
+
+    public ApiService()
+    {
+        _connectionString =
+            "Data Source=localhost, 1433; User=SA; Password=yourStrong()Password; Initial Catalog = master; Integrated Security=False;Connect Timeout=30;Encrypt=False";
+    }
+    public async Task<int> AddProductWarehouseAsync(DeliveryDTO delivery)
+    {
+        using var connection = new SqlConnection(_connectionString);
+        await connection.OpenAsync();
+        
+        if(delivery.IdProduct <= 0 || delivery.IdWarehouse <= 0) throw new Exception("Ids must be greater than zero");
+        
+        //checking if product with given id exists
+        var checkProduct = new SqlCommand(
+            "SELECT 1 FROM Product WHERE IdProduct = @IdProduct", connection);
+        checkProduct.Parameters.AddWithValue("@IdProduct", delivery.IdProduct);
+        var checkResult = await checkProduct.ExecuteScalarAsync();
+        if(checkResult == null) throw new Exception("No product with such id found");
+
+        //checking if warehouse with given id exists
+        var checkWarehouse = new SqlCommand(
+            "SELECT 1 FROM warehouse WHERE IdWarehouse = @IdWarehouse", connection);
+        checkWarehouse.Parameters.AddWithValue("@IdWarehouse", delivery.IdWarehouse
+        );
+        var checkResultWarehouse = await checkWarehouse.ExecuteScalarAsync();
+        if(checkResultWarehouse == null) throw new Exception("No warehouse with such id found");
+        
+        //checking if there is record in order table with such idproduct and amount
+        var checkRecord = new SqlCommand(
+            "SELECT IdOrder FROM [order] WHERE IdProduct = @IdProduct AND Amount = @Amount AND CreatedAt < @CreatedAt", connection);
+        checkRecord.Parameters.AddWithValue("@IdProduct", delivery.IdProduct);
+        checkRecord.Parameters.AddWithValue("@Amount", delivery.Amount);
+        checkRecord.Parameters.AddWithValue("@CreatedAt", delivery.CreatedAt);
+        var orderId = (int?) await checkRecord.ExecuteScalarAsync();
+        if(orderId == null) throw new Exception("No order that matches the request");
+        
+        //checking whether this order has not been completed
+        var checkCompletion = new SqlCommand(
+            "SELECT 1 FROM product_warehouse WHERE IdOrder = @IdOrder", connection);
+        checkCompletion.Parameters.AddWithValue("@IdOrder", orderId);
+        var checkResultCompletion = await checkCompletion.ExecuteScalarAsync();
+        if(checkResultCompletion != null) throw new Exception("Order is already completed");
+        
+        //updating order date
+        var updateOrderDate = new SqlCommand(
+            "UPDATE [order] SET FulfilledAt = @FulfilledAt WHERE IdOrder = @IdOrder", connection);
+        updateOrderDate.Parameters.AddWithValue("@FulfilledAt", DateTime.Now);
+        updateOrderDate.Parameters.AddWithValue("@IdOrder", orderId);
+        await updateOrderDate.ExecuteNonQueryAsync();
+        
+        //getting price
+        var getPrice = new SqlCommand(
+            "SELECT Price FROM product WHERE IdProduct = @IdProduct", connection);
+        getPrice.Parameters.AddWithValue("@IdProduct", delivery.IdProduct);
+        var price = (decimal?) await getPrice.ExecuteScalarAsync();
+        
+        //inserting
+        var insertRecord = new SqlCommand(
+            "INSERT INTO Product_Warehouse (IdWarehouse, IdProduct, IdOrder, Amount, Price, CreatedAt) OUTPUT INSERTED.IdProductWarehouse VALUES (@IdWarehouse, @IdProduct, @IdOrder, @Amount, @Price, @CreatedAt)", connection);
+        insertRecord.Parameters.AddWithValue("@IdWarehouse", delivery.IdWarehouse);
+        insertRecord.Parameters.AddWithValue("@IdProduct", delivery.IdProduct);
+        insertRecord.Parameters.AddWithValue("@IdOrder", orderId);
+        insertRecord.Parameters.AddWithValue("@Amount", delivery.Amount);
+        insertRecord.Parameters.AddWithValue("@Price", price * delivery.Amount);
+        insertRecord.Parameters.AddWithValue("@CreatedAt", delivery.CreatedAt);
+        
+        var id = (int) await insertRecord.ExecuteScalarAsync();
+        return id!;
+    }
 }
